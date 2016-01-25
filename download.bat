530393a74fe437c06d1807eca9d2b29bd0254cc0
Dmitry Ponyatov <dponyatov@gmail.com>
2016-01-25 12:38:58 +0400

    * down
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- download.bat
+++ download.bat
@@ -1,5 +1,7 @@
-start wget --no-check-certificate -c https://build.openmodelica.org/omc/builds/windows/releases/1.9.3/OpenModelica-v1.9.3.exe
-
 mkdir doc
 start wget --no-check-certificate -O doc/Fritz.pdf -c https://www.openmodelica.org/images/docs/tutorials/modelicatutorialfritzson.pdf
 start wget --no-check-certificate -c -O doc/Sjolund.pdf http://liu.diva-portal.org/smash/get/diva2:614273/FULLTEXT01.pdf
+
+wget --no-check-certificate -c https://build.openmodelica.org/omc/builds/windows/releases/1.9.3/OpenModelica-v1.9.3.exe
+
+git pull
